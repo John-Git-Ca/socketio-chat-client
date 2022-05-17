48af2aac1df0032e3149079291e9f5f22eb2ba13
John <john.lizuhua@outlook.com>
2022-05-16 22:40:32 -0400

    add video call function

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import {
   Button,
   Row,
@@ -19,13 +19,17 @@ import {
   BiUser,
   BiArrowBack,
   BiSend,
+  BiVideo,
 } from 'react-icons/bi';
+import Peer from 'simple-peer';
+import './notification.css';
 
 // const ENDPOINT = 'http://localhost:5000/';
 const ENDPOINT = 'https://john-socketio-chat-server.herokuapp.com/';
 let socket;
 
 const Chat = () => {
+  const [me, setMe] = useState('');
   const { name, room } = useParams();
   const [users, setUsers] = useState([]);
   const [editMessage, setEditMessage] = useState('');
@@ -33,12 +37,38 @@ const Chat = () => {
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
   const [showUsers, setShowUsers] = useState(false);
 
+  const [stream, setStream] = useState();
+  const [call, setCall] = useState({});
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [callEnded, setCallEnded] = useState(false);
+  const myVideo = useRef();
+  const userVideo = useRef();
+  const connectionRef = useRef();
+
   useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((currentStream) => {
+        setStream(currentStream);
+        console.log(currentStream);
+        myVideo.current.srcObject = currentStream;
+      })
+      .catch((err) => {
+        console.log('err:' + err);
+      });
+
     socket = io(ENDPOINT);
+    socket.on('me', (id) => {
+      setMe(id);
+    });
     socket.emit('join', { name, room }, (error) => {
       if (error) {
         // alert(error);
       }
+    });
+
+    socket.on('callUser', ({ from, name: callerName, signal }) => {
+      setCall({ isReceivingCall: true, from, name: callerName, signal });
     });
 
     // return () => {
@@ -56,6 +86,51 @@ const Chat = () => {
       setUsers(users);
     });
   }, []);
+
+  const VideoCall = (userId) => {
+    const peer = new Peer({ initiator: true, trickle: false, stream });
+
+    peer.on('signal', (data) => {
+      socket.emit('callUser', {
+        userToCall: userId,
+        signalData: data,
+        from: me,
+        name,
+      });
+    });
+
+    peer.on('stream', (currentStream) => {
+      userVideo.current.srcObject = currentStream;
+    });
+
+    socket.on('callAccepted', (signal) => {
+      setCallAccepted(true);
+      peer.signal(signal);
+      console.log('call accepted');
+    });
+
+    connectionRef.current = peer;
+
+    console.log(peer);
+  };
+
+  const answerCall = () => {
+    setCallAccepted(true);
+    const peer = new Peer({ initiator: false, trickle: false, stream });
+    console.log('answering call');
+    peer.on('signal', (data) => {
+      console.log('answering call');
+      socket.emit('answerCall', { signal: data, to: call.from });
+    });
+
+    peer.on('stream', (currentStream) => {
+      userVideo.current.srcObject = currentStream;
+    });
+
+    peer.signal(call.signal);
+
+    connectionRef.current = peer;
+  };
 
   const handleSendMessage = (e) => {
     e.preventDefault();
@@ -123,10 +198,19 @@ const Chat = () => {
               ></FormControl>
             </Col>
           </Row>
-          <Row className="m-0 d-flex justify-content-between">
+          <Row className="m-0 d-flex justify-content-end">
             <Col xs={1} sm={1} className="h-100 emojipicker p-0">
               <Button variant="" className="w-100 h-100" id="btn">
                 <BiWinkSmile id="btn2" size={25} />
+              </Button>
+            </Col>
+            <Col xs={2} sm={2} className="h-100 p-0">
+              <Button
+                variant=""
+                className="sendbtn"
+                onClick={() => setShowUsers(true)}
+              >
+                <BiVideo id="text2" size={30} />
               </Button>
             </Col>
             <Col xs={2} sm={2} className="h-100 p-0">
@@ -139,18 +223,44 @@ const Chat = () => {
               </Button>
             </Col>
           </Row>
+          {call && call.isReceivingCall && !callAccepted && (
+            <div className="notification row justify-content-around">
+              <div className="col-12 fs-6 name">
+                Video Call from: {call.name}
+              </div>
+              <div className="col-3 btn accept" onClick={answerCall}>
+                Accept
+              </div>
+              <div className="col-3 btn reject">Reject</div>
+            </div>
+          )}
           {showEmojiPicker && (
             <div className="emojimart">
               <Picker id="btn3" onEmojiClick={onEmojiClick} />
             </div>
           )}
           {showUsers && (
-            <div className="users">
+            <div className="users rounded">
               {users &&
                 users.map((user, index) => (
-                  <ListGroupItem key={index}>{user.name}</ListGroupItem>
+                  <ListGroupItem key={index}>
+                    <div className="row">
+                      <div className="col">{user.name}</div>
+
+                      <BiVideo
+                        className="col btn m-0 p-0"
+                        id="text2"
+                        onClick={() => VideoCall(user.id)}
+                        size={20}
+                      />
+                    </div>
+                  </ListGroupItem>
                 ))}
             </div>
+          )}
+          {stream && <video playsInline autoPlay muted ref={myVideo}></video>}
+          {callAccepted && !callEnded && (
+            <video playsInline autoPlay ref={userVideo}></video>
           )}
         </Col>
       </Row>
